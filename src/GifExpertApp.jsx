6f1710150405e9c893d8e986d44b271e3824b3e4
Stefano <stefanop21@outlook.es>
2023-11-07 16:02:03 -0500

    S6 - GifExpert first web app

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+    const onAddCategory = () => {
+        setCategories(['Valorant', ...categories]);
+    };
+
+    return (
+        <>
+            {/* Título */}
+            <h1>GifExpertApp</h1>
+
+            {/* Input */}
+            <AddCategory setCategories={setCategories} />
+
+            {/* Listado de Gif */}
+            {/* <button onClick={onAddCategory}>Agregar</button> */}
+            <ol>
+                {categories.map((category) => {
+                    return <li key={category}>{category}</li>
+                })}
+            </ol>
+
+            {/* Gif Iteam */}
+        </>
+    );
+};
