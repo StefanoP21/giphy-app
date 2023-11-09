04ff324a4baa86d070b3f5b3e24bec8c04f29e4e
Stefano <stefanop21@outlook.es>
2023-11-08 22:23:13 -0500

    Fetch API - Implementación de la función getGifs()

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,8 +1,11 @@
+import { getGifs } from '../helpers/getGifs';
+
 export const GifGrid = ({ category }) => {
+  getGifs(category);
+
   return (
     <>
       <h3>{category}</h3>
-      <p>Hola</p>
     </>
   );
 };
