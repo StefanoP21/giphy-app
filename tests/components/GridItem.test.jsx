78564f37e7ebef50d763d6c0be46ac852d969c22
Stefano <stefanop21@outlook.es>
2023-11-10 18:28:10 -0500

    Prueba 1 en el componente AddCategory

--- tests/components/GridItem.test.jsx
+++ tests/components/GridItem.test.jsx
@@ -1,12 +1,26 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GridItem } from '../../src/components/GridItem';
 
-describe('Pruebas en <GridTem />', () => {
+describe('Pruebas en <GridItem />', () => {
   const title = 'Dark Souls';
   const url = 'https://dark-souls.com/dark-souls.jpg';
 
-  test('Evaluar el snapshot', () => {
+  test('debe de hacer match con el snapshot', () => {
     const { container } = render(<GridItem title={title} url={url} />);
     expect(container).toMatchSnapshot();
+  });
+
+  test('debe de mostrar la imagen con el URL y el ALT indicado', () => {
+    render(<GridItem title={title} url={url} />);
+    // screen.debug();
+    // expect(screen.getByRole('img').src).toBe(url);
+    const { src, alt } = screen.getByRole('img');
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+
+  test('debe de mostrar el título en el componente', () => {
+    render(<GridItem title={title} url={url} />);
+    expect(screen.getByText(title)).toBeTruthy();
   });
 });
