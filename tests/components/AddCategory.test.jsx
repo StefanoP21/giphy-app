14e9bba8ae58f79c8f0764e2d1452b3439c5bf4c
Stefano <stefanop21@outlook.es>
2023-11-10 21:45:24 -0500

    Prueba 2 en el componente AddCategory

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -9,4 +9,18 @@ describe('Pruebas en <AddCategory />', () => {
     fireEvent.input(input, { target: { value: 'Saitama' } });
     expect(input.value).toBe('Saitama');
   });
+
+  test('debe de llamar onNewCategory si el input tiene un valor', () => {
+    const inputValue = 'Valorant';
+    // TODO: ????
+    render(<AddCategory onNewCategory={() => {}} />);
+
+    const input = screen.getByRole('textbox');
+    const form = screen.getByRole('form');
+
+    fireEvent.input(input, { target: { value: inputValue } });
+    fireEvent.submit(form);
+    // screen.debug();
+    expect(input.value).toBe('');
+  });
 });
