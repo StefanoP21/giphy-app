f24e749c3140b9f48ff1f7eacf0b134a085f3edd
Stefano <stefanop21@outlook.es>
2023-11-16 20:08:34 -0500

    Prueba 2 en el componente GifGrid

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,12 +1,43 @@
 import { render, screen } from '@testing-library/react';
 import { GifGrid } from '../../src/components';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Pruebas en <GifGrid />', () => {
   const category = 'Dark Souls';
 
   test('debe de mostrar el loading incialmente', () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
+
     render(<GifGrid category={category} />);
     expect(screen.getByText('Cargando...'));
     expect(screen.getByText(category));
+  });
+
+  test('debe de mostrar items cuando se cargan las imágenes useFetchGifs', () => {
+    const gifs = [
+      {
+        id: 'abc',
+        title: 'One Punch',
+        img: 'https://lcalhost/one-punch.jpg',
+      },
+      {
+        id: 'def',
+        title: 'Valorant',
+        img: 'https://lcalhost/valorant.jpg',
+      },
+    ];
+
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false,
+    });
+
+    render(<GifGrid category={category} />);
+    expect(screen.getAllByRole('img').length).toBe(2);
   });
 });
